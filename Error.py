cb5d0796b6eedd141bb9f06bc308103948c131c7
Vlad Kishkin <vladkishkin1@yandex.ru>
2021-03-23 04:31:49 +0300

    add error for Fraction

--- Error.py
+++ Error.py
@@ -4,12 +4,44 @@
 
 
 class Error(Exception):
+    """
+    Общие ошибки библиотеки calculatorLobachevsky
+    """
 
-    def __init__(self, text):
+    def __init__(self, text: str, arguments: dict):
         self.text = text
+
+    def __str__(self):
+        result = '{} ('.format(self.text)
+        for key, value in self.arguments.items():
+            result = '{}{}={}, '.format(result, key, value)
+        result = '{})'.format(result[:-2])
+
+        return result
+
+
+class FractionError(Error):
+    """
+    Ошибки связанные с работой с дробью
+    """
+
+    def __init__(self, text: str, arguments: dict):
+        self.text = text
+        self.arguments = arguments
+
+    def __str__(self):
+        result = '{} ('.format(self.text)
+        for key, value in self.arguments.items():
+            result = '{}{}={}, '.format(result, key, value)
+        result = '{})'.format(result[:-2])
+
+        return result
 
 
 class MatrixError(Error):
+    """
+    Ошибки связанные с работой с матрицами
+    """
 
     def __init__(self, text: str, arguments: dict):
         self.text = text
