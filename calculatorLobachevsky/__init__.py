2abc9aa2c6b20fcc1a69e37be349714c731817b1
Kishkin-Vlad <VladKishkin1@yandex.ru>
2021-04-20 02:21:08 +0300

    add __init.py files

---
+++ calculatorLobachevsky/__init__.py
@@ -0,0 +1,6 @@
+from .Error import *
+from .matrix import Matrix
+from .fraction import Fraction
+from .polynom import _Monom, Polynom
+from .expression import _Lexem, Expression
+
---
+++ tests.py
@@ -0,0 +1,7 @@
+import sys
+import os
+sys.path.insert(0, '{}/tests'.format(sys.path[0]))
+print(sys.path)
+from tests.test
+
+mon = testPolynom.Monom()
