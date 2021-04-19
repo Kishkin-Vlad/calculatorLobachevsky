275734cf54331e34c0eb1e7fe07d5b0c333fd455
Kishkin-Vlad <VladKishkin1@yandex.ru>
2021-04-20 02:25:07 +0300

    init setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='calculateLobachevsky',
+      version='0.1',
+      url='https://github.com/Kishkin-Vlad/calculatorLobachevsky',
+      license='',
+      author='Vladislav Kishkin',
+      author_email='',
+      description='',
+      packages=find_packages(exclude=['']),
+      long_description=open('README.md').read(),
+      zip_safe=False)
