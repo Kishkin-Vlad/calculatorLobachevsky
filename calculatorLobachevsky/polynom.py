b6cc3c1fe21c94f6e7e9b61e636aa9bf6e71ec1e
Kishkin-Vlad <VladKishkin1@yandex.ru>
2021-04-20 04:57:13 +0300

    add funcs for monom: init, get_coef, get_power, get_var, eq, ne, lt, le. And tests for them

--- calculatorLobachevsky/polynom.py
+++ calculatorLobachevsky/polynom.py
@@ -12,7 +12,205 @@ class _Monom(object):
     Класс моном
     """
 
-    def __init__(self, coefficient=0, *args):
+    def __init__(self, coefficient=0, variables=None):
+        """
+        :param coefficient: коэффициент монома
+        :param variables: словарь с:
+        ключи - название перемернных
+        значениями - степенями переменных
+        """
+
+        if not isinstance(coefficient, (int, float, complex)):
+            raise PolynomError('coefficient is not a number',
+                               {'def': '__init__(for _Monom)', 'type': type(coefficient)})
+        self.coefficient = coefficient
+
+        if not (isinstance(variables, dict) or variables is None):
+            raise PolynomError('variables is not a dict',
+                               {'def': '__init__(for _Monom)', 'type': type(variables)})
+        self.variables = {}
+        if variables is not None:
+            keys = variables.keys()
+            for key in keys:
+                if not isinstance(variables[key], (int, float, complex)):
+                    raise PolynomError('variable is not a number',
+                                       {'def': '__init__(for _Monom)', 'type': type(variables[key])})
+                self.variables[key] = variables[key]
+
+    def get_coefficient(self):
+        """
+        Получение коэффициента монома
+        :return: коэффициент монома
+        """
+
+        return self.coefficient
+
+    def get_power(self, variables):
+        """
+        Получение степени переменной
+        :param variables: строка с названием переменной (или список строк)
+        :return: степень переменной
+        """
+
+        if not isinstance(variables, (str, list)):
+            raise PolynomError('variables is not a str or a list',
+                               {'def': 'get_power(for _Monom)', 'type': type(variables)})
+
+        if isinstance(variables, str):
+            try:
+                return self.variables[variables]
+            except KeyError:
+                raise PolynomError('no such variable',
+                                   {'def': 'get_power(for _Monom)', 'variable': variables})
+
+        if isinstance(variables, list):
+            result = []
+            for variable in variables:
+                try:
+                    result.append(self.variables[variable])
+                except KeyError:
+                    raise PolynomError('no such variable',
+                                       {'def': 'get_power(for _Monom)', 'variable': variable})
+            return result
+
+    def get_variables(self):
+        """
+        Получение всех переменных с их степенями
+        :return: словарь с:
+        ключами - названиями переменных
+        значениями - степенями переменных
+        """
+
+        return self.variables
+
+    def __eq__(self, other):
+        """
+        Проверка на равенство - self == other
+        :param other: моном или число
+        :return: True - равны, False - не равны
+        """
+
+        if isinstance(other, _Monom):
+            # Если равны коэф и переменные
+            return self.get_coefficient() == other.get_coefficient() and self.get_variables() == other.get_variables()
+        elif isinstance(other, (int, float, complex)):
+            # Если нет переменных в мономе и коэф. монома равен числу
+            return not self.get_variables() and self.get_coefficient() == other
+        else:
+            raise PolynomError('comparing a number with an incompatible type',
+                               {'def': '__eq__(for _Monom)', 'type': type(other)})
+
+    def __ne__(self, other):
+        """
+        Проверка на неравенство - self != other
+        :param other: моном или число
+        :return: True - не равны, False - равны
+        """
+
+        if isinstance(other, _Monom):
+            # Если равны коэф и переменные
+            return self.get_coefficient() != other.get_coefficient() or self.get_variables() != other.get_variables()
+        elif isinstance(other, (int, float, complex)):
+            # Если нет переменных в мономе и коэф. монома равен числу
+            return self.get_variables() or self.get_coefficient() != other
+        else:
+            raise PolynomError('comparing a number with an incompatible type',
+                               {'def': '__ne__(for _Monom)', 'type': type(other)})
+
+    def __lt__(self, other):
+        """
+        Сравнение - self < other
+        :param other: моном или число
+        :return: True => self < other, False => self > other
+        """
+
+        if isinstance(other, _Monom):
+            var1 = self.get_variables()
+            var2 = other.get_variables()
+
+            keys1 = sorted(var1)
+            keys2 = sorted(var2)
+            len1 = len(keys1)
+            len2 = len(keys2)
+            for i, (key1, key2) in enumerate(zip(keys1, keys2)):
+                if key1 == key2:
+                    if (i == len1 - 1 or i == len2 - 1) and len1 == len2:
+                        if var1[key1] < var2[key2]:
+                            return True
+                        return False
+                    else:
+                        if var1[key1] > var2[key2]:
+                            return False
+                elif key1 > key2:
+                    return False
+
+            if len1 < len2:
+                return True
+            elif len1 > len2:
+                return False
+            if self.get_coefficient() >= other.get_coefficient():
+                return False
+
+            return True
+        elif isinstance(other, (int, float, complex)):
+            # Если нет переменных в мономе и коэф. монома равен числу
+            return not self.get_variables() and self.get_coefficient() < other
+        else:
+            raise PolynomError('comparing a number with an incompatible type',
+                               {'def': '__lt__(for _Monom)', 'type': type(other)})
+
+    def __le__(self, other):
+        """
+        Сравнение - self <= other
+        :param other: моном или число
+        :return: True => self <= other, False => self >= other
+        """
+
+        if isinstance(other, _Monom):
+            var1 = self.get_variables()
+            var2 = other.get_variables()
+
+            keys1 = sorted(var1)
+            keys2 = sorted(var2)
+            len1 = len(keys1)
+            len2 = len(keys2)
+            for i, (key1, key2) in enumerate(zip(keys1, keys2)):
+                if key1 == key2:
+                    if (i == len1 - 1 or i == len2 - 1) and len1 == len2:
+                        if var1[key1] <= var2[key2]:
+                            return True
+                        return False
+                    else:
+                        if var1[key1] > var2[key2]:
+                            return False
+                elif key1 > key2:
+                    return False
+
+            if len1 < len2:
+                return True
+            elif len1 > len2:
+                return False
+            if self.get_coefficient() > other.get_coefficient():
+                return False
+
+            return True
+        elif isinstance(other, (int, float, complex)):
+            # Если нет переменных в мономе и коэф. монома равен числу
+            return not self.get_variables() and self.get_coefficient() <= other
+        else:
+            raise PolynomError('comparing a number with an incompatible type',
+                               {'def': '__lt__(for _Monom)', 'type': type(other)})
+
+    def __gt__(self, other):
+        """
+        Сравнение - self > other
+        :param other: моном или число
+        :return: True => self > other, False => self > other
+        """
+
+        pass
+
+    def __ge__(self, other):
         pass
 
 
--- dirtests/testPolynom.py
+++ dirtests/testPolynom.py
@@ -1,8 +1,216 @@
 import unittest
-from calculatorLobachevsky import polynom
+from calculatorLobachevsky.polynom import _Monom, Polynom, PolynomError
 
 
 class TestPolynom(unittest.TestCase):
 
-    def test_(self):
-        pass
+    def test_init_numbers(self):
+        """
+        Инициализация чисел
+        """
+
+        _Monom(1)
+        _Monom(1.0)
+        _Monom(complex(1, 2))
+
+    def test_init_with_variables(self):
+        """
+        Инициализация с переменными
+        """
+
+        _Monom(2, {'x': 1})
+        _Monom(3, {'y': 2, 'z': 3})
+        _Monom(10, {'param1': 20, 'param2': 30})
+
+    def test_init_number_with_error(self):
+        """
+        Попытка инициализировать число с передачей не числа
+        """
+
+        self.assertRaises(PolynomError, _Monom.__init__, self, '10')
+        self.assertRaises(PolynomError, _Monom.__init__, self, [10])
+        self.assertRaises(PolynomError, _Monom.__init__, self, {10: 20})
+
+    def test_get_coef(self):
+        """
+        Получение коэффициента
+        """
+
+        monom = _Monom(10, {'x': 1, 'y': 2})
+        self.assertEqual(monom.get_coefficient(), 10)
+
+        monom = _Monom(20)
+        self.assertEqual(monom.get_coefficient(), 20)
+
+    def test_get_power(self):
+        """
+        Получение степени переменных
+        """
+
+        monom = _Monom(1, {'x': 2, 'y': 3})
+        self.assertEqual(monom.get_power('x'), 2)
+        self.assertEqual(monom.get_power('y'), 3)
+        self.assertEqual(monom.get_power(['x', 'y']), [2, 3])
+
+    def test_get_power_with_error(self):
+        """
+        Попытка получить переменных, которых нет
+        """
+
+        monom = _Monom(1, {'x': 2})
+        self.assertRaises(PolynomError, _Monom.get_power, monom, 'y')
+        self.assertRaises(PolynomError, _Monom.get_power, monom, ['x', 'y'])
+        self.assertRaises(PolynomError, _Monom.get_power, monom, ['y', 'z'])
+        self.assertRaises(PolynomError, _Monom.get_power, monom, 1)
+
+    def test_monom_equal_monom(self):
+        """
+        Проверка равенства мономов
+        """
+
+        mon1 = _Monom(10)
+        mon2 = _Monom(10)
+        self.assertEqual(mon1, mon2)
+
+        mon1 = _Monom(10, {'x': 1})
+        mon2 = _Monom(10, {'x': 1})
+        self.assertEqual(mon1, mon2)
+
+        mon1 = _Monom(10, {'x': 1, 'y': 2})
+        mon2 = _Monom(10, {'y': 2, 'x': 1})
+        self.assertEqual(mon1, mon2)
+
+    def test_monom_not_equal_monom(self):
+        """
+        Проверка неравенства мономов
+        """
+
+        mon1 = _Monom(10)
+        mon2 = _Monom(20)
+        self.assertNotEqual(mon1, mon2)
+
+        mon1 = _Monom(10)
+        mon2 = _Monom(10, {'x': 1})
+        self.assertNotEqual(mon1, mon2)
+
+    def test_monom_equal_numbers(self):
+        """
+        Проверка равенства монома и числа
+        """
+
+        mon = _Monom(10)
+        num = 10
+        self.assertEqual(mon, num)
+
+        mon = _Monom(20.5)
+        num = 20.5
+        self.assertEqual(mon, num)
+
+        mon = _Monom(30)
+        num = complex(30, 0)
+        self.assertEqual(mon, num)
+
+    def test_monom_not_equal_numbers(self):
+        """
+        Проверка неравенства монома и числа
+        """
+
+        mon = _Monom(10)
+        num = 20
+        self.assertNotEqual(mon, num)
+
+        mon = _Monom(10.1)
+        num = _Monom(10.2)
+        self.assertNotEqual(mon, num)
+
+        mon = _Monom(10)
+        num = complex(10, 1)
+        self.assertNotEqual(mon, num)
+
+        mon = _Monom(10, {'x': 1})
+        num = _Monom(10)
+        self.assertNotEqual(mon, num)
+
+    def test_monom_lt_monom(self):
+        """
+        Сравнение мономов - <
+        """
+
+        mon1 = _Monom(10)
+        mon2 = _Monom(20)
+        self.assertEqual(mon1 < mon2, True)
+
+        mon1 = _Monom(10)
+        mon2 = _Monom(10, {'x': 1})
+        self.assertEqual(mon1 < mon2, True)
+
+        mon1 = _Monom(10, {'x': 1})
+        mon2 = _Monom(10, {'x': 1, 'y': 2})
+        self.assertEqual(mon1 < mon2, True)
+
+        mon1 = _Monom(10, {'x': 1, 'y': 1})
+        mon2 = _Monom(10, {'x': 1, 'y': 2})
+        self.assertEqual(mon1 < mon2, True)
+
+        mon1 = _Monom(10, {'y': 1, 'x': 1})
+        mon2 = _Monom(10, {'x': 1, 'y': 2})
+        self.assertEqual(mon1 < mon2, True)
+
+    def test_monom_lt_number(self):
+        """
+        Сравнение монома и числа - <
+        """
+
+        mon = _Monom(10)
+        num = 20
+        self.assertEqual(mon < num, True)
+
+        mon = _Monom(10)
+        num = 10
+        self.assertEqual(mon < num, False)
+
+        mon = _Monom(5, {'x': 1})
+        num = 10
+        self.assertEqual(mon < num, False)
+
+    def test_monom_le_monom(self):
+        """
+        Сравнение мономов - <=
+        """
+
+        mon1 = _Monom(10)
+        mon2 = _Monom(20)
+        self.assertEqual(mon1 <= mon2, True)
+
+        mon1 = _Monom(10)
+        mon2 = _Monom(10, {'x': 1})
+        self.assertEqual(mon1 <= mon2, True)
+
+        mon1 = _Monom(10, {'x': 1})
+        mon2 = _Monom(10, {'x': 1, 'y': 2})
+        self.assertEqual(mon1 <= mon2, True)
+
+        mon1 = _Monom(10, {'x': 1, 'y': 1})
+        mon2 = _Monom(10, {'x': 1, 'y': 2})
+        self.assertEqual(mon1 <= mon2, True)
+
+        mon1 = _Monom(10, {'y': 2, 'x': 1})
+        mon2 = _Monom(10, {'x': 1, 'y': 2})
+        self.assertEqual(mon1 <= mon2, True)
+
+    def test_monom_le_number(self):
+        """
+        Сравнение монома и числа - <=
+        """
+
+        mon = _Monom(10)
+        num = 20
+        self.assertEqual(mon <= num, True)
+
+        mon = _Monom(10)
+        num = 10
+        self.assertEqual(mon <= num, True)
+
+        mon = _Monom(5, {'x': 1})
+        num = 10
+        self.assertEqual(mon <= num, False)
